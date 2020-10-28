d1dc24b5292348d172b63d79d957c08853c9837d
João Ceia <joao.p.ceia@gmail.com>
2020-10-28 23:01:49 +0000

    v2

--- LaymanHFTrader/LaymanHFTrader.cpp
+++ LaymanHFTrader/LaymanHFTrader.cpp
@@ -1,7 +1,27 @@
+//
+// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/boostorg/beast
+//
+
+//------------------------------------------------------------------------------
+//
+// Example: WebSocket SSL client, synchronous
+//
+//------------------------------------------------------------------------------
+
+#include "root_certificates.hpp"
+
 #include <boost/beast/core.hpp>
+#include <boost/beast/ssl.hpp>
 #include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/stream.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <string>
@@ -10,6 +30,7 @@ namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 // Sends a WebSocket message and prints the response
@@ -21,9 +42,9 @@ int main(int argc, char** argv)
         if (argc != 4)
         {
             std::cerr <<
-                "Usage: websocket-client-sync <host> <port> <text>\n" <<
+                "Usage: websocket-client-sync-ssl <host> <port> <text>\n" <<
                 "Example:\n" <<
-                "    websocket-client-sync echo.websocket.org 80 \"Hello, world!\"\n";
+                "    websocket-client-sync-ssl echo.websocket.org 443 \"Hello, world!\"\n";
             return EXIT_FAILURE;
         }
         std::string host = argv[1];
@@ -33,20 +54,37 @@ int main(int argc, char** argv)
         // The io_context is required for all I/O
         net::io_context ioc;
 
+        // The SSL context is required, and holds certificates
+        ssl::context ctx{ ssl::context::tlsv12_client };
+
+        // This holds the root certificate used for verification
+        load_root_certificates(ctx);
+
         // These objects perform our I/O
         tcp::resolver resolver{ ioc };
-        websocket::stream<tcp::socket> ws{ ioc };
+        websocket::stream<beast::ssl_stream<tcp::socket>> ws{ ioc, ctx };
 
         // Look up the domain name
         auto const results = resolver.resolve(host, port);
 
         // Make the connection on the IP address we get from a lookup
-        auto ep = net::connect(ws.next_layer(), results);
+        auto ep = net::connect(get_lowest_layer(ws), results);
+
+        // Set SNI Hostname (many hosts need this to handshake successfully)
+        if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str()))
+            throw beast::system_error(
+                beast::error_code(
+                    static_cast<int>(::ERR_get_error()),
+                    net::error::get_ssl_category()),
+                "Failed to set SNI Hostname");
 
         // Update the host_ string. This will provide the value of the
         // Host HTTP header during the WebSocket handshake.
         // See https://tools.ietf.org/html/rfc7230#section-5.4
         host += ':' + std::to_string(ep.port());
+
+        // Perform the SSL handshake
+        ws.next_layer().handshake(ssl::stream_base::client);
 
         // Set a decorator to change the User-Agent of the handshake
         ws.set_option(websocket::stream_base::decorator(
