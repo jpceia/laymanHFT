8e6f9f04329dfb81ce2813a9e8d1d31e919695c4
João Ceia <joao.p.ceia@gmail.com>
2020-10-30 01:15:43 +0000

    removed header comments

--- LaymanHFT/LaymanHFT.cpp
+++ LaymanHFT/LaymanHFT.cpp
@@ -1,19 +1,3 @@
-//
-// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-// Official repository: https://github.com/boostorg/beast
-//
-
-//------------------------------------------------------------------------------
-//
-// Example: WebSocket SSL client, synchronous
-//
-//------------------------------------------------------------------------------
-
-
 #include <boost/beast/core.hpp>
 #include <boost/beast/ssl.hpp>
 #include <boost/beast/websocket.hpp>
